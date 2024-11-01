92598556e5517d9a31da546085ef5fc0b50eb952
podoshva <podoshvaaa@gmail.com>
2024-11-01 09:25:49 +0500

    reset button added

--- index.js
+++ index.js
@@ -1,47 +1,70 @@
 'use strict';
 const gameFieldHtml = document.getElementById('game-field');
+const activeActionHtml = document.getElementById('active-action');
+const resetButtonHtml = document.getElementById('reset-button');
 const gameFieldMatrix = [
 	[1, 2, 3],
 	[4, 5, 6],
 	[7, 8, 9],
 ];
-let activeAction = 'x'; // initial value
 
-gameFieldMatrix.forEach((row, rowIndex) => {
-	row.forEach((column, columnIndex) => {
-		const fieldCell = document.createElement('div');
-		fieldCell.className = 'field-cell';
-		fieldCell.id = column;
+const init = () => {
+	let activeAction = 'x'; // initial value
+	activeActionHtml.innerText = activeAction;
+	gameFieldMatrix.forEach((row, rowIndex) => {
+		row.forEach((column, columnIndex) => {
+			const fieldCell = document.createElement('div');
+			fieldCell.className = 'field-cell';
+			fieldCell.id = column;
 
-		fieldCell.addEventListener('mousedown', () => {
-			if (column != 'o' && column != 'x') {
-				column = activeAction;
-				console.log(`coordinates: ${rowIndex} ${columnIndex}`);
-				if ((rowIndex + columnIndex) % 2 == 0 || rowIndex + columnIndex == 0) {
-					console.log('можно провести диагональ');
-				} else {
-					console.log('диагональ провести нельзя');
+			fieldCell.addEventListener('mousedown', () => {
+				if (column != 'o' && column != 'x') {
+					column = activeAction;
+					console.log(`coordinates: ${rowIndex} ${columnIndex}`);
+					if (
+						(rowIndex + columnIndex) % 2 == 0 ||
+						rowIndex + columnIndex == 0
+					) {
+						console.log('можно провести диагональ');
+					} else {
+						console.log('диагональ провести нельзя');
+					}
+					row[columnIndex] = activeAction;
+					const actionImg = document.createElement('img');
+					actionImg.className = 'action';
+					if (activeAction == 'x') {
+						actionImg.src = './images/cross.svg';
+						activeAction = 'o';
+					} else {
+						actionImg.src = './images/zero.svg';
+						activeAction = 'x';
+					}
+					activeActionHtml.innerText = activeAction;
+					fieldCell.appendChild(actionImg);
 				}
-				row[columnIndex] = activeAction;
-				const actionImg = document.createElement('img');
-				actionImg.className = 'action';
-				if (activeAction == 'x') {
-					actionImg.src = './images/cross.svg';
-					activeAction = 'o';
-				} else {
-					actionImg.src = './images/zero.svg';
-					activeAction = 'x';
-				}
-				fieldCell.appendChild(actionImg);
-			}
-			console.log(gameFieldMatrix);
-			return;
+				console.log(gameFieldMatrix);
+				return;
+			});
+			gameFieldHtml.appendChild(fieldCell);
 		});
-		gameFieldHtml.appendChild(fieldCell);
 	});
-});
-
-const Cell = initialValue => {
-	this.initialValue = initialValue;
-	this.value = initialValue;
 };
+
+const reset = () => {
+	let counter = 1;
+	for (let i = 0; i < gameFieldMatrix.length; i++) {
+		for (let j = 0; j < gameFieldMatrix[i].length; j++) {
+			const actionImg = document.getElementById(counter);
+			actionImg.remove();
+			gameFieldMatrix[i][j] = counter;
+			counter++;
+		}
+	}
+	init();
+};
+
+init();
+
+resetButtonHtml.addEventListener('click', () => {
+	reset();
+});
